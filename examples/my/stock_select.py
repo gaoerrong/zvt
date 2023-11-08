78c37778b3d6127f231cf91edf45a52cda7b9299
高二荣 <zhen.zi@winrobot360.com>
2023-11-08 20:47:45 +0800

    update

--- examples/my/stock_select.py
+++ examples/my/stock_select.py
@@ -6,6 +6,9 @@ from zvt.domain import (
 )
 import datetime
 import pandas as pd
+import mplfinance as mpf
+import os
+
 
 def stock_select(strategy_type: int, **p_kv):
     # 从db中查询多少天的数据
@@ -13,6 +16,10 @@ def stock_select(strategy_type: int, **p_kv):
     user_data_size = 5
     # 获取当前日期
     today = datetime.date.today()
+    # 创建文件夹
+    output_dir = f"/Users/gaoerrong/Downloads/stock_select_result/{p_kv['sub_dir_path']}/{today.strftime('%Y-%m-%d')}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     # 计算往前推 n 天的日期
     delta = datetime.timedelta(days=n)
     start_timestamp = today - delta
@@ -36,12 +43,42 @@ def stock_select(strategy_type: int, **p_kv):
         if matched:
             new_data = pd.DataFrame([{'code': row['code'], 'name': row['name']}])
             matched_data = pd.concat([matched_data, new_data], ignore_index=True)
+            # 输出k线图
+            file_name = f"{output_dir}/{row['code']}_{row['name']}.png"
+            plot_candlestick(k_data_list, title=f"{row['code']}_{row['name']}", output_filename=file_name)
             print(f"符合条件,code:{row['code']},name:{row['name']}")
+        else:
+            continue
 
     if not matched_data.empty:
-        matched_data.to_excel('/Users/gaoerrong/Downloads/select_result.xlsx', index=False)
+        select_result_file_name = f"{output_dir}/{p_kv['entity_type']}_select_result.xlsx"
+        matched_data.to_excel(select_result_file_name, index=False)
 
+# 绘制K线图
+def plot_candlestick(kline_data, title, output_filename):
+    # 解析K线数据
+    df_new = kline_data[['timestamp', 'open', 'high', 'low', 'close', 'volume']]
+    # df_new['timestamp'] = pd.to_datetime(df_new['timestamp'])
+    # df_new.loc[:, 'timestamp'] = pd.to_datetime(df_new['timestamp'])
+    df_new.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 
+    # 设置日期为索引
+    df_new.set_index('Date', inplace=True)
+    # 调用make_marketcolors函数，定义K线颜色
+    mc = mpf.make_marketcolors(
+        up="red",  # 上涨K线的颜色
+        down="green",  # 下跌K线的颜色
+        edge="black",  # 蜡烛图箱体的颜色
+        volume="blue",  # 成交量柱子的颜色
+        wick="black"  # 蜡烛图影线的颜色
+    )
+
+    # 调用make_mpf_style函数，自定义图表样式
+    # 函数返回一个字典，查看字典包含的数据，按照需求和规范调整参数
+    style = mpf.make_mpf_style(base_mpl_style="ggplot", marketcolors=mc)
+    # 绘制蜡烛图
+    mpf.plot(df_new, type='candle', title=title, style=style, volume=True, mav=(5,10,20), ylabel="price", figratio=(12, 6),
+             savefig=output_filename)
 
 
 # 波段选股方式，先有几天的下降，然后当天比前一天的收盘价要高
@@ -75,10 +112,11 @@ def wave_band_select(k_data_list):
 
 #均线 5>10>20 or 10>20>30
 
+# 最近几天之内有一天的成交量特别大。需要接下来一段时间去关注。
 
 if __name__ == "__main__":
     print("start select stock...")
     # a股板块选股
-    wave_bandselect(1,entity_type='block', data_schema=Block1dKdata, order = Block1dKdata.timestamp.asc())
+    stock_select(1, entity_type='block', data_schema=Block1dKdata, order=Block1dKdata.timestamp.asc(), sub_dir_path='a_block')
     # a股正股选股
-    wave_bandselect(1, entity_type='stock', data_schema=Stock1dKdata, order=Stock1dKdata.timestamp.asc())
+    # stock_select(1, entity_type='stock', data_schema=Stock1dKdata, order=Stock1dKdata.timestamp.asc(), sub_dir_path='a_stock')
--- examples/my/test.py
+++ examples/my/test.py
@@ -1,23 +1,53 @@
+import pandas as pd
+import mplfinance as mpf
 
-from zvt.contract.api import  get_data
-from zvt.domain import (
-    Stock1dHfqKdata,
-)
-import datetime
-import numpy as np
+def plot_candlestick(kline_data, output_filename):
+    # 解析K线数据
+    # 假设K线数据是一个包含日期、开盘价、最高价、最低价、收盘价和交易量的列表
+    dates = [data[0] for data in kline_data]
+    open_prices = [data[1] for data in kline_data]
+    high_prices = [data[2] for data in kline_data]
+    low_prices = [data[3] for data in kline_data]
+    close_prices = [data[4] for data in kline_data]
+    volumes = [data[5] for data in kline_data]
 
-# 获取当前日期
-today = datetime.date.today()
+    # 创建DataFrame
+    df = pd.DataFrame({
+        'Date': pd.to_datetime(dates),
+        'Open': open_prices,
+        'High': high_prices,
+        'Low': low_prices,
+        'Close': close_prices,
+        'Volume': volumes
+    })
 
-# 计算往前推 7 天的日期
-delta = datetime.timedelta(days=7)
-start_timestamp = today - delta
+    # 设置日期为索引
+    df.set_index('Date', inplace=True)
+    # 调用make_marketcolors函数，定义K线颜色
+    mc = mpf.make_marketcolors(
+        up="red",  # 上涨K线的颜色
+        down="green",  # 下跌K线的颜色
+        edge="black",  # 蜡烛图箱体的颜色
+        volume="blue",  # 成交量柱子的颜色
+        wick="black"  # 蜡烛图影线的颜色
+    )
 
-stock_day_data_list = get_data(
-    data_schema=Stock1dHfqKdata,code='xxx',
-    order=Stock1dHfqKdata.timestamp.asc()
-)
-# 计算均值
-vol_ma = np.mean(stock_day_data_list)
+    # 调用make_mpf_style函数，自定义图表样式
+    # 函数返回一个字典，查看字典包含的数据，按照需求和规范调整参数
+    style = mpf.make_mpf_style(base_mpl_style="ggplot", marketcolors=mc)
+    # 绘制蜡烛图
+    mpf.plot(df, type='candle', style=style, volume=True, ylabel="price", figratio=(12, 6), savefig=output_filename)
 
+# 示例用法
+kline_data = [
+    ['2022-01-01', 100, 120, 80, 110,10009],
+    ['2022-01-02', 110, 130, 100, 120,10009],
+    ['2022-01-03', 120, 140, 110, 130,10009],
+['2022-01-04', 120, 140, 110, 130,10009],
+['2022-01-05', 120, 140, 110, 130,10009],
+['2022-01-06', 120, 140, 110, 130,10009],
+    # ...更多的K线数据
+]
 
+output_file = "/Users/gaoerrong/Downloads/candlestick_chart.png"
+plot_candlestick(kline_data, output_file)
